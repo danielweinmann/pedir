3f12ecabc4ed2eaea047b092176f33aafefc12ca
Daniel Weinmann <danielweinmann@gmail.com>
2015-01-07 16:05:07 -0200

    Adds Amanda Palmer's video

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
   config.action_mailer.default_url_options = { :host => 'pedir.vc' }
   config.action_mailer.asset_host = 'http://pedir.vc'
 
